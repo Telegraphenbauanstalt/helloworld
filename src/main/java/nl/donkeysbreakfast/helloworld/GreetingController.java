eba16fc431e32d276cef2d8e5cd9c9cee172166d
rincewind <nobody@example.com>
2020-09-19 09:42:50 +0200

    init
    initializr mit NB plugin
    Tutorial https://spring.io/guides/gs/rest-service/

---
+++ src/main/java/nl/donkeysbreakfast/helloworld/GreetingController.java
@@ -0,0 +1,23 @@
+package nl.donkeysbreakfast.helloworld;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Telegraphenbauanstalt (https://github.com/Telegraphenbauanstalt)
+ */
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+    }
+
+}
